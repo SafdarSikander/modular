1c42dfb8543f8ac7281f702d4d54fce64f3c9118
Safdar Sikander <safdar_sikander@live.com>
2023-04-09 23:44:14 +0500

    Project init

---
+++ src/modules/Auth/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Auth(): JSX.Element {
+  return (
+    <View>
+      <Text>Modular</Text>
+    </View>
+  );
+}
+
+export default Auth;
